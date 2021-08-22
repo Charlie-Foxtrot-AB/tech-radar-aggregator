951a9d0c5ef94ca7edefd77b0e41727f8cb5cb6c
Rickard Hinsell <rickard@hinsell.se>
2021-08-22 19:45:37 +0200

    Moved rest of thoughtworks code

--- collect.py
+++ collect.py
@@ -1,18 +1,6 @@
 import dataSources.thoughtWorks
-from docs import category, generateDocs
+from docs import generateDocs
 
-quarters = [
-    ('Techniques', 'https://www.thoughtworks.com/radar/techniques', 'Tech Radar - Techniques'),
-    ('Platforms', 'https://www.thoughtworks.com/radar/platforms', 'Tech Radar - Platforms'),
-    ('Tools', 'https://www.thoughtworks.com/radar/tools', 'Tech Radar - Tools'),
-    ('Languages and frameworks', 'https://www.thoughtworks.com/radar/languages-and-frameworks', 'Tech Radar - Languages-and-frameworks')
-]
-
-categories = []
-
-for idx, part in enumerate(quarters):
-    newCategory = category(part[0], [])
-    newCategory.results = dataSources.thoughtWorks.getData(part[1], part[2])
-    categories.append( newCategory )
+categories = dataSources.thoughtWorks.getTechList()
 
 generateDocs(categories)
--- dataSources/thoughtWorks.py
+++ dataSources/thoughtWorks.py
@@ -1,5 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from docs import category
+
+quarters = [
+    ('Techniques', 'https://www.thoughtworks.com/radar/techniques', 'Tech Radar - Techniques'),
+    ('Platforms', 'https://www.thoughtworks.com/radar/platforms', 'Tech Radar - Platforms'),
+    ('Tools', 'https://www.thoughtworks.com/radar/tools', 'Tech Radar - Tools'),
+    ('Languages and frameworks', 'https://www.thoughtworks.com/radar/languages-and-frameworks', 'Tech Radar - Languages-and-frameworks')
+]
+categories = []
+
+def getTechList():
+    for part in quarters:
+        newCategory = category(part[0], [])
+        newCategory.results = getData(part[1], part[2])
+        categories.append( newCategory )
+    return categories
 
 def getData(url, eventCategory):
     r = requests.get(url)
