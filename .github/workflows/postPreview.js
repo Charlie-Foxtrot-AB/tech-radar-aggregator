ebff4d2405acaa3d15c11b8ddb6b91eb30031ccb
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2021-09-03 11:03:35 +0200

    fix: try patching file path in script

--- .github/workflows/postPreview.js
+++ .github/workflows/postPreview.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = function(github, context) {
-    const preview = fs.readFileSync('../docs/index.md').toString();
+    const preview = fs.readFileSync('../../docs/index.md').toString();
     github.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
