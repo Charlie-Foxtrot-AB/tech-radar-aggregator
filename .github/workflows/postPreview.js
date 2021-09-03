b3b01bf198ad471c54b00b3aad2271c777533616
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2021-09-03 11:23:35 +0200

    feat #11: Add previews as comments in PR (#12)
    * feat #11: Add previews as comments in PR

---
+++ .github/workflows/postPreview.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+module.exports = function({github, context}) {
+    const preview = fs.readFileSync('./docs/index.md').toString();
+    github.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: `<details><summary>Output preview:</summary>${preview}</details>`
+      })
+}
