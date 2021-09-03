2137161c2bbf77289a4e8969d4f624f975aff13d
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2021-09-03 11:07:01 +0200

    fix: I love javascript, especially when there is no linting

--- .github/workflows/postPreview.js
+++ .github/workflows/postPreview.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-module.exports = function(github, context) {
+module.exports = function({github, context}) {
     const preview = fs.readFileSync('./docs/index.md').toString();
     github.issues.createComment({
         issue_number: context.issue.number,
